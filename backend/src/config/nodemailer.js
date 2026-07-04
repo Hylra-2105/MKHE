5a59d37cfbba855c38a589a2001e12ca519b29a9
Hylres <230941130+Hylra-2105@users.noreply.github.com>
2026-07-04 12:13:21 +0700

    Fix gmail v3

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -7,9 +7,8 @@ export const getTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      requireTLS: true,
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
